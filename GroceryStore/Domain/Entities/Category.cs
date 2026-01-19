971dfe2c3e6237318906129ea80c9e2d3db2da8b
9asmodey6 <longjorney1@gmail.com>
2026-01-19 20:40:16 +0200

    Domain Layer Changes

--- GroceryStore/Domain/Entities/Category.cs
+++ GroceryStore/Domain/Entities/Category.cs
@@ -6,5 +6,7 @@ public class Category : BaseEntity
 {
     required public string Name { get; set; }
 
+    public int ParentId { get; private set; }
+
     public ICollection<Product> Products { get; set; } = new List<Product>();
 }
