22aabfd7756b06fb98bb2d979d2ba7b7f236c342
9asmodey6 <longjorney1@gmail.com>
2026-01-19 20:21:53 +0200

    .

--- GroceryStore/Domain/Entities/Attribute.cs
+++ GroceryStore/Domain/Entities/Attribute.cs
@@ -14,8 +14,4 @@ public class Attribute : BaseEntity
     public int MinValue { get; private set; }
 
     public int MaxValue { get; private set; }
-
-    public string? Value { get; private set; }
-
-
 }
